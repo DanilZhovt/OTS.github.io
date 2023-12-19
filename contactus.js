dcd82a8eb8b12f9cd6d399247d4dde77b5b4ebd8
Groza1337 <serezgvozdkov@mail.ru>
2023-12-20 00:59:33 +0300

    апро

--- contactus.js
+++ contactus.js
@@ -8,16 +8,16 @@ let weirdlettercollection = "1BDE0A7C3F893B90A2D371DEE3602DB7BF40";
 let wrong = document.getElementById("wrong");
 let sent = document.getElementById("sent");
 
-function sendEmail(){
+
+
+function sendTelega(){
+		console.log(message.value)
+		text = message.value
+		url=`https://api.telegram.org/bot6809638221:AAFmSOPVuXOaqo1AgQHDD8XL0H9X4jOA0cE/sendMessage?chat_id=-1002074351363&text=${text}`;
+		fetch(url)
     if(!mail.value.localeCompare(truemail.value)){
         wrong.classList.remove('active');
-        Email.send({
-            SecureToken : "3b588c44-1ddf-466c-b8d2-207a7aeb752d",
-            To : 'serezgvozdkov@mail.ru',
-            From : 'VirmLeed@gmail.com',
-            Subject : type + ' // ' + subject.value,
-            Body : message.value + '\n // ' + truemail.value,
-        }).then(/*message => alert(message)*/);
+        fetch(url).then(/*message => alert(message)*/);
         sent.classList.add('active');
         form.reset();
         return false;
@@ -27,3 +27,10 @@ function sendEmail(){
         return false;
     }
 }
+/*Email.send({
+            SecureToken : "3b588c44-1ddf-466c-b8d2-207a7aeb752d",
+            To : 'serezgvozdkov@mail.ru',
+            From : 'VirmLeed@gmail.com',
+            Subject : type + ' // ' + subject.value,
+            Body : message.value + '\n // ' + truemail.value,
+        })*/
---
+++ test.js
@@ -0,0 +1,3 @@
+text = "NEGRS"
+url=`https://api.telegram.org/bot6809638221:AAFmSOPVuXOaqo1AgQHDD8XL0H9X4jOA0cE/sendMessage?chat_id=-1002074351363&text=${text}`;
+fetch(url)
